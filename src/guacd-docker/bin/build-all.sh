766275c065aefd090405e9172225f77ca0aa5866
Dedy Martadinata S <dedyms@proton.me>
2022-10-27 10:45:45 +0700

    fix case

--- src/guacd-docker/bin/build-all.sh
+++ src/guacd-docker/bin/build-all.sh
@@ -99,9 +99,12 @@ install_from_git() {
 # Build and install core protocol library dependencies
 #
 case "${TARGETPLATFORM}" in
-  "linux/amd64") install_from_git "https://github.com/FreeRDP/FreeRDP" "$WITH_FREERDP" $FREERDP_OPTS;;
-  "linux/arm64" | "" ) install_from_git "https://github.com/FreeRDP/FreeRDP" "$WITH_FREERDP" $FREERDP_OPTS;;
+  "linux/amd64") 
+      install_from_git "https://github.com/FreeRDP/FreeRDP" "$WITH_FREERDP" $FREERDP_OPTS;;
+  "linux/arm64" | "" ) 
+      install_from_git "https://github.com/FreeRDP/FreeRDP" "$WITH_FREERDP" $FREERDP_OPTS_ARM;;
   *)
+      echo "Unknown TARGETPLATFORM"
 install_from_git "https://github.com/libssh2/libssh2" "$WITH_LIBSSH2" $LIBSSH2_OPTS
 install_from_git "https://github.com/seanmiddleditch/libtelnet" "$WITH_LIBTELNET" $LIBTELNET_OPTS
 install_from_git "https://github.com/LibVNC/libvncserver" "$WITH_LIBVNCCLIENT" $LIBVNCCLIENT_OPTS
